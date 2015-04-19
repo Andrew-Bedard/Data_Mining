0f9481c7ee4a658fcb6832bb14fb2a8465d67825
Jan <artag.malsagov@gmail.com>
2015-04-19 12:56:03 +0200

    report augmented with MAE vs MSE

---
+++ assignment_1/MAE_vs_MAE_regress/Regression.py
@@ -0,0 +1,10 @@
+# Data imported from http://vincentarelbundock.github.io/Rdatasets/csv/datasets/longley.csv
+
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+
+df = pd.read_csv('longley.csv', index_col=0)
+
+
