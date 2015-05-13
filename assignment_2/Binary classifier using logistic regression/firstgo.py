48d1acf04c773364e6dcc587c18ca1cd8e54a334
Andrew Bedard <andrew.bedard@student.uva.nl>
2015-05-13 23:11:25 +0200

    so much stuff

--- assignment_2/Binary classifier using logistic regression/firstgo.py
+++ assignment_2/Binary classifier using logistic regression/firstgo.py
@@ -8,13 +8,15 @@ Created on Tue May 12 13:22:18 2015
 import pandas as pd
 import numpy as np
 from patsy import dmatrices
-import sklearn as sk
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import cross_val_score
+from sklearn import metrics
 
 """ Load data set """
 data = pd.read_csv('decimated_training_set_10_pct.csv')
 data = data.drop('date_time',1)
-data = data.drop(data.columns[26:50], 1)
+#ata = data.drop(data.columns[26:50], 1)
 
 
 """ Adding and congragating features """
@@ -41,6 +43,8 @@ data['price_diff'] = dummy_hist_ad - dummy_price
 
 
 #starrating_diff = visitor_hist_starrating - prop_starrating
+dummy_fill = data.visitor_hist_starrating.mean()
+data.visitor_hist_starrating = data.visitor_hist_starrating.fillna(dummy_fill)
 dummy_hstar = data['visitor_hist_starrating']
 dummy_pstar = data['prop_starrating']
 data['starrating_diff'] = dummy_hstar - dummy_pstar
@@ -53,11 +57,10 @@ f = lambda x: x.fillna(x.quantile(0.25))
 data.prop_location_score2 = data.groupby('prop_country_id')["prop_location_score2"].transform(f)
 
 
-
-
 col_list = list(data.columns.values)
 
-""" Down sample negative instances """
+grouped = data.groupby('prop_country_id')
+
 
 """Lets try some stuff"""
 
@@ -78,3 +81,50 @@ model = LogisticRegression(class_weight = 'auto')
 model = model.fit(X,y)
 
 model.score(X,y)
+
+# evaluate the model by splitting into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+model2 = LogisticRegression(class_weight = 'auto')
+model2.fit(X_train, y_train)
+
+# predict class labels for the test set
+predicted = model2.predict(X_test)
+print(predicted)
+
+# generate class probabilities
+probs = model2.predict_proba(X_test)
+print(probs)
+
+# generate evaluation metrics
+print( metrics.accuracy_score(y_test, predicted))
+print( metrics.roc_auc_score(y_test, probs[:, 1]))
+
+print( metrics.confusion_matrix(y_test, predicted))
+print( metrics.classification_report(y_test, predicted))
+
+# evaluate the model using 10-fold cross-validation
+scores = cross_val_score(LogisticRegression(class_weight = 'auto'), X, y, scoring='accuracy', cv=10)
+print(scores)
+print(scores.mean())
+
+
+#ref_df = data[['srch_id', 'booking_bool', 'click_bool']]
+#ref_df['real_score'] = (ref_df.booking_bool * 5  + ref_df.click_bool)
+#ref_df['predict_booking'] = booking_predictions
+#ref_df['predict_click'] = click_predictions
+#ref_df['predict_score'] = ref_df.predict_booking * 5 + ref_df.predict_click
+#grouped = ref_df.groupby('srch_id')
+#
+#ndcgs = []
+#for name, group in grouped:
+#    real_sorted = group.sort('real_score', ascending=False)
+#    idcg = 0
+#    for (i, (index,val)) in enumerate(real_sorted.iterrows(), start=1):
+#        idcg += (2**val.real_score-1)/numpy.log2(i+1)
+#    predict_sorted = group.sort('predict_score', ascending=False)
+#    dcg = 0
+#    for (i, (index,val)) in enumerate(predict_sorted.iterrows(), start=1):
+#        dcg += (2**val.real_score-1)/np.log2(i+1)
+#    ndcgs.append(dcg/idcg)
+#
+#print(np.mean(ndcgs))
